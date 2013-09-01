04226c2e71b935154cb1c048d5b35176ae56b492
Jason Gonzales <jason.gonzales23@gmail.com>
2013-09-01 15:14:21 -0700

    big commit bad

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -61,3 +61,63 @@ var servicesCircle = {
     }
 }
 
+$('.newsletter-link').on('click', function() {
+  newWindow = window.open('http://visitor.constantcontact.com/d.jsp?m=1101135603319&p=oi',
+                          'mailWindow', 'toolbar,location,menubar,scrollbars,resizable,width=628,height=333');
+});
+
+function openit_gc() {
+  newWindow = window.open('https://www.securedata-trans8.com/ap/ap_ui_v2/includes/gift_certificates_v2.php?name_link=openskybodyworksltd',
+                          'gcWindow', 'toolbar,location,menubar,scrollbars,resizable,width=800,height=600');
+}
+
+var defaultOptions = 'toolbar,location,menubar,scrollbars,resizable,width=800,height=600';
+
+function open_stuff(e, options){
+  options = options || defaultOptions;
+  var target = e.target;
+  newWindow = window.open(target.href, target.title, options);
+  return false;
+}
+
+$('a[title="gcWindow"]').on('click', function(evt) {
+  options= 'toolbar,location,menubar,scrollbars,resizable,width=800,height=600';
+
+  evt.preventDefault();
+  open_stuff(evt, options)
+});
+
+
+function initialize() {
+        var geocoder = new google.maps.Geocoder();
+        var myLatlng = new google.maps.LatLng(39.9886, -83.04216);
+        var address = '1334 W 5th Ave, Columbus, OH 43212';
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              map.setCenter(results[0].geometry.location);
+              var marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location
+              });
+            } else {
+              var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: 'Open Sky Day Spa'
+              });
+              console.log('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+
+      }
+google.maps.event.addDomListener(window, 'load', initialize);
+
